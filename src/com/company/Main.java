80ea51e771a56109bffe6c6ecaecb2db3b5fd63d
Никита Ким <kim.n@it41.cube>
2022-11-13 14:06:01 +1200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        int[] mas = new int[100];
+//        int[] mas1 = {23, 43, 75, 1};
+//        System.out.println(mas1[0]);
+//        System.out.println(mas1[1]);
+//        System.out.println(mas1[2]);
+//        System.out.println(mas1[3]);
+//        int i = 0;
+//        while (i < mas.length) {
+//            mas[i] = 1;
+//            i++;
+//
+//        }
+//        int j = 0;
+//        while (j < mas.length) ;
+//        {
+//            System.out.println(mas1[i]);
+//            j++;
+//
+//
+//        }
+        int n = 28;
+        int[] mas2 = new int[n];
+        int[] mas3 = {0, 48, 50, 72, 84};
+
+        int wewe = 0;
+        while (wewe < 4) {
+            System.out.println(mas3[wewe]);
+            wewe++;
+
+        }
+
+    }
+}
